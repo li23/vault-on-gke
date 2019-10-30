ee801c2d29694fbe9c036a20ccd7378cb0eed5b9
tom chisholm <45635027+ignwtomc@users.noreply.github.com>
2019-10-30 12:55:19 -0700

    Update plan_runner.sh

--- plan_runner.sh
+++ plan_runner.sh
@@ -2,4 +2,4 @@
 
 cd terraform
 terraform init
-TF_VAR_org_id=943433058474 TF_VAR_billing_account=01160D-C9D630-DC571C terraform plan
+TF_VAR_org_id=<Your-ORG-ID> TF_VAR_billing_account=<Your-billing-ID> terraform plan
